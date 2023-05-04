312a57f1b1748331dae23f6da97b4ae638c1add5
pikachiu <132340402+pika-0203@users.noreply.github.com>
2023-05-05 01:54:31 +0900

    Update StatisticsRankingUpdaterRepository.php

--- app/Models/Repositories/StatisticsRankingUpdaterRepository.php
+++ app/Models/Repositories/StatisticsRankingUpdaterRepository.php
@@ -24,7 +24,7 @@ class StatisticsRankingUpdaterRepository implements StatisticsRankingUpdaterRepo
         /**
          *  メンバー１０人以上のオープンチャットが対象 
          *  直近１週間で最小のメンバー数と、現在のメンバー数を比較して、増加%と差を取得する。
-         *  差 + (増加% / 10) を `index1` として降順にソートする。
+         *  差 + 増加率 * 10 を `index1` として降順にソートする。
          */
         DB::execute(
             'INSERT INTO
