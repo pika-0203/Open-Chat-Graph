e75bbb17a5a654033db55f607d32aae6ac587a8b
pikachiu <132340402+pika-0203@users.noreply.github.com>
2023-05-05 22:38:05 +0900

    Update StatisticsRankingUpdaterRepository.php

--- app/Models/Repositories/StatisticsRankingUpdaterRepository.php
+++ app/Models/Repositories/StatisticsRankingUpdaterRepository.php
@@ -15,98 +15,17 @@ class StatisticsRankingUpdaterRepository implements StatisticsRankingUpdaterRepo
 
     private function executeUpdateCreate(): int
     {
-        // テーブルを初期化
-        DB::$pdo->exec(
-            'DELETE FROM statistics_ranking'
-        );
+        $sqlFile = __DIR__ . '/sql/StatisticsRankingUpdaterRepository_updateCreateRankingTable.sql';
+        $sqlQueries = explode(';', file_get_contents($sqlFile));
 
-        /**
-         * 昨日〜7日前の最小メンバー数と、最新メンバー数を比較して、差と増減%をランキングテーブルに挿入する。
-         * 差 + (増減% / 10) を`index1`カラムに挿入する。
-         * メンバー１０人以上のオープンチャットが対象
-         */
-        DB::$pdo->exec(
-            'INSERT INTO
-                statistics_ranking (
-                    id,
-                    open_chat_id,
-                    diff_member,
-                    percent_increase,
-                    index1
-                )
-            SELECT
-                t1.open_chat_id,
-                t1.open_chat_id,
-                t1.member - t2.member as diff_member,
-                ((t1.member - t2.member) / t2.member) * 100 as percent_increase,
-                (
-                    (t1.member - t2.member) + (((t1.member - t2.member) / t2.member) * 10)
-                ) as index1
-            FROM
-                (
-                    SELECT
-                        st.open_chat_id,
-                        (
-                            SELECT
-                                member
-                            FROM
-                                statistics
-                            WHERE
-                                open_chat_id = st.open_chat_id
-                            ORDER BY
-                                `date` DESC
-                            LIMIT
-                                1
-                        ) AS member
-                    FROM
-                        statistics as st
-                    WHERE
-                        st.member >= 10
-                    GROUP BY
-                        st.open_chat_id
-                ) t1
-                LEFT JOIN (
-                    SELECT
-                        open_chat_id,
-                        MIN(member) as member
-                    FROM
-                        statistics
-                    WHERE
-                        `date` >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
-                        AND `date` <= (
-                            SELECT
-                                DATE_SUB(MAX(`date`), INTERVAL 1 DAY)
-                            FROM
-                                statistics
-                            WHERE
-                                open_chat_id = statistics.open_chat_id
-                        )
-                        AND member >= 10
-                    GROUP BY
-                        open_chat_id
-                ) t2 ON t1.open_chat_id = t2.open_chat_id
-            ORDER BY
-                index1 DESC;'
-        );
+        foreach ($sqlQueries as $query) {
+            if (trim($query) === '') {
+                continue;
+            }
 
-        // `index1`カラムを降順でソートして、idをその順番で振る
-        DB::$pdo->exec(
-            'SET @row_number := 0;'
-        );
-        return DB::execute(
-            'UPDATE
-                statistics_ranking
-                JOIN (
-                    SELECT
-                        id,
-                        (@row_number := @row_number + 1) AS new_id
-                    FROM
-                        statistics_ranking
-                    ORDER BY
-                        index1 DESC
-                ) subquery ON statistics_ranking.id = subquery.id
-            SET
-                statistics_ranking.id = subquery.new_id;'
-        )->rowCount();
+            $rowCount = DB::$pdo->exec($query);
+        }
+
+        return $rowCount;
     }
 }
