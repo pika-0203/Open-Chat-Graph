d79061a4a4c82fa10f6009b3a941a280715bfa4d
pikachiu <132340402+pika-0203@users.noreply.github.com>
2023-05-05 22:41:55 +0900

    Update StatisticsRankingUpdaterRepository_updateCreateRankingTable.sql

--- app/Models/Repositories/sql/StatisticsRankingUpdaterRepository_updateCreateRankingTable.sql
+++ app/Models/Repositories/sql/StatisticsRankingUpdaterRepository_updateCreateRankingTable.sql
@@ -1,6 +1,11 @@
 DELETE FROM
     statistics_ranking;
 
+/*
+ * 昨日〜7日前の最小メンバー数と、最新メンバー数を比較して、差と増減%をランキングテーブルに挿入する。
+ * 差 + (増減% / 10) を`index1`カラムに挿入する。
+ * メンバー１０人以上のオープンチャットが対象
+ */
 INSERT INTO
     statistics_ranking (
         id,
