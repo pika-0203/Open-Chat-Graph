094531094a6bdd199e2a070584890dc33fe17912
pikachiu <132340402+pika-0203@users.noreply.github.com>
2023-05-27 04:36:04 +0900

    Cron的なAPIにコメントを追加

--- app/Controllers/Api/CronApiController.php
+++ app/Controllers/Api/CronApiController.php
@@ -4,48 +4,85 @@ declare(strict_types=1);
 
 namespace App\Controllers\Api;
 
+use App\Services\OpenChat\Cron;
+use App\Models\Repositories\UpdateOpenChatRepositoryInterface;
+use App\Models\Repositories\StatisticsRankingUpdaterRepositoryInterface;
+use App\Models\Repositories\OpenChatListRepositoryInterface;
+use App\Config\AppConfig;
+use App\Config\OpenChatCrawlerConfig;
+use App\Services\OpenChat\AddOpenChat;
+use App\Services\Sitemap;
+use App\Models\Repositories\LogRepositoryInterface;
+
 class CronApiController
 {
+    /**
+     * オープンチャット更新のAPI (500件ずつ処理)
+     */
     function index(
-        \App\Services\OpenChat\Cron $statisticsCron,
-        \App\Models\Repositories\StatisticsRankingUpdaterRepositoryInterface $rankingUpdater,
-        \App\Services\Statistics\OpenChatStatisticsRanking $openChatStatsRanking
+        Cron $statisticsCron,
+        LogRepositoryInterface $logRepository,
     ) {
-        response(['cron' => 'executed'])->send();
-
+        // レスポンスを返して切断する
+        response(['updateOpenChatCron' => 'executed'])->send();
         fastcgi_finish_request();
 
-        $statisticsCron->handle();
-
-        $rankingUpdater->updateCreateRankingTable();
-
-        $rankingList = $openChatStatsRanking->get(1, 10);
-        saveArrayToFile(\App\Config\AppConfig::FILEPATH_TOP_RANKINGLIST, $rankingList + ['updatedAt' => time()]);
+        try {
+            $statisticsCron->handle();
+        } catch (\Throwable $e) {
+            $logRepository->logUpdateOpenChatError(0, 0, 'null', 'null', $e->getMessage());
+        }
 
         exit;
     }
 
-    function ocrowcount(\App\Models\Repositories\UpdateOpenChatRepositoryInterface $updateRepository)
+    /**
+     * 本日更新対象のレコード数を返すAPI
+     */
+    function ocrowcount(UpdateOpenChatRepositoryInterface $updateRepository)
     {
-        $idArray = $updateRepository->getOpenChatIdByPeriod(time(), \App\Config\AppConfig::CRON_EXECUTE_COUNT);
+        $idArray = $updateRepository->getOpenChatIdByPeriod(9999999);
         return response(['openChatRowCount' => count($idArray)]);
     }
 
+    /**
+     * ランキング更新のAPI
+     */
     function rank(
-        \App\Models\Repositories\StatisticsRankingUpdaterRepositoryInterface $rankingUpdater,
-        \App\Services\Statistics\OpenChatStatisticsRanking $openChatStatsRanking
+        StatisticsRankingUpdaterRepositoryInterface $rankingUpdater,
+        OpenChatListRepositoryInterface $openChatRepository,
+        Sitemap $sitemap
     ) {
         $resultRowCount = $rankingUpdater->updateCreateRankingTable();
-        $rankingList = $openChatStatsRanking->get(1, 10);
-        saveArrayToFile(\App\Config\AppConfig::FILEPATH_TOP_RANKINGLIST, $rankingList + ['updatedAt' => time()]);
+
+        // トップページのキャッシュファイルを生成する
+        $rankingList = [];
+        $rankingList['openChatList'] = $openChatRepository->findMemberStatsRanking(0, 10);
+        $rankingList['updatedAt'] = time();
+
+        foreach ($rankingList['openChatList'] as &$oc) {
+            // 説明文を半角140文字以内にする
+            $oc['description'] = mb_strimwidth($oc['description'], 0, 140, '…',);
+        }
+
+        saveArrayToFile(AppConfig::FILEPATH_TOP_RANKINGLIST, $rankingList);
+
+        // サイトマップを更新する
+        $sitemap->updateSitemap();
+
         return response(['rankingUpdaterResultCount' => $resultRowCount]);
     }
 
-    function addoc(\App\Services\OpenChat\AddOpenChat $openChat, string $url)
+    /**
+     * オープンチャット追加のAPI(管理者用)
+     */
+    function addoc(AddOpenChat $openChat, string $url)
     {
-        $isValidUrl = fn ($url) => preg_match(\App\Config\OpenChatCrawlerConfig::LINE_URL_MATCH_PATTERN, $url);
+        // URL文字列を検証
+        $isValidUrl = fn ($url) => preg_match(OpenChatCrawlerConfig::LINE_URL_MATCH_PATTERN, $url);
 
         if (!$isValidUrl($url)) {
+            // LINEのURLではない場合、リダイレクト先のURLを調べる
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -55,6 +92,7 @@ class CronApiController
             curl_close($ch);
 
             if (!$isValidUrl($url)) {
+                // リダイレクト先も無効なURLの場合
                 return response([
                     'message' => '無効なURLです。',
                     'id' => null
@@ -62,6 +100,7 @@ class CronApiController
             }
         }
 
+        // オープンチャットを追加
         $result = $openChat->add($url);
         return response($result);
     }
