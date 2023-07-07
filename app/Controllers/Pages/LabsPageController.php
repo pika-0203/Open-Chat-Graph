d5a1ac64c04f8ba7d5d004aac417ae7b7225b75c
pikachiu <132340402+pika-0203@users.noreply.github.com>
2023-07-08 06:48:25 +0900

    Add files via upload

--- app/Controllers/Pages/LabsPageController.php
+++ app/Controllers/Pages/LabsPageController.php
@@ -12,7 +12,7 @@ class LabsPageController
         $desc = "試作中の機能をLabs（ラボ）として提供しています。";
         $_meta = meta()->setTitle($name)->setDescription($desc)->setOgpDescription($desc);
 
-        $_css = ['room_page_28', 'site_header_21', 'site_footer_18', 'labs_03'];
+        $_css = ['room_page_28', 'site_header_21', 'site_footer_18', 'labs_04'];
 
         return view('statistics/labs_content', compact('_meta', '_css'));
     }
@@ -20,10 +20,11 @@ class LabsPageController
     function live()
     {
         $name = 'ライブトーク利用時間分析ツール';
-        $desc = "トーク履歴を読み込ませることで、ライブトークの利用時間をグラフと共に確認することができます。";
-        $_meta = meta()->setTitle($name)->setDescription($desc)->setOgpDescription($desc);
+        $desc = "オープンチャットのトーク履歴から、ライブトークの通話時間・開催メンバーをグラフで表示することができます。";
+        $ogpDesc = "トーク履歴から、ライブトークの利用時間をグラフで表示することができます。";
+        $_meta = meta()->setTitle($name)->setDescription($desc)->setOgpDescription($ogpDesc);
 
-        $_css = ['room_page_28', 'site_header_21', 'site_footer_18', 'live_ana_03'];
+        $_css = ['room_page_28', 'site_header_21', 'site_footer_18', 'live_ana_06'];
 
         return view('statistics/live_content', compact('_meta', '_css'));
     }
--- app/Views/statistics/live_content.php
+++ app/Views/statistics/live_content.php
@@ -7,21 +7,11 @@
     <?php statisticsComponent('site_header') ?>
     <!-- オープンチャット表示ヘッダー -->
     <main class="openchat unset hidden-graph" id="graph-area">
-        <header class="openchat-header unset">
+        <section class="openchat-header unset">
             <h1 class="talkroom_link_h1 unset" id="octitle">ライブトーク利用時間分析ツール</span></h1>
-            <p class="page-desc">トーク履歴を読み込ませることで、ライブトークの利用時間をグラフと共に確認することができます。<br>iOS・Android版LINEのトーク履歴（テキストファイル）に対応しています。</p>
-            <p class="page-desc">ブラウザ上で分析処理を行うので、サーバーにトーク履歴が送信されることはありません。</p>
-            <p class="page-desc">
-                <time>2023/7/4</time>
-                <small>公開</small>
-                <br>
-                <time>2023/7/5</time>
-                <small>0:00〜9:59間のログが無視されるバグを修正</small>
-                <br>
-                <time>2023/7/6</time>
-                <small>iOS版に対応</small>
-            </p>
-        </header>
+            <p class="page-desc">トーク履歴からライブトークの利用時間をグラフで表示します。</p>
+            <hr class="page-desc">
+        </section>
         <!-- グラフセクション -->
         <div class="graph-area">
             <div class="graph-title">
@@ -41,7 +31,7 @@
                         <th class="table-date">日付</th>
                         <th>回数</th>
                         <th>時間</th>
-                        <th class="table-user">開催ユーザー</th>
+                        <th class="table-user">開始したメンバー</th>
                     </tr>
                 </thead>
                 <tbody id="live-table-body">
@@ -49,8 +39,23 @@
             </table>
         </div>
         <form>
+            <legend class="small-font">テキスト形式のトーク履歴を選択</legend>
             <input id="file-input" type="file" accept="text/plain">
+            <p class="form-errorMessage" id="errorMessage"></p>
+            <small>端末上で読み込むため、トーク履歴が当サイトに送信されることはありません。</small>
         </form>
+        <p class="page-desc small-font">iOS・Android版LINEで保存されたテキスト形式のトーク履歴に対応しています。</p>
+        <p class="page-desc small-font">トーク履歴の保存方法は<a href="https://help.line.me/line/ios/sp?lang=ja&contentId=20007388" rel="external nofollow noopener">LINEヘルプ</a>を参照してください。</p>
+        <p class="page-desc small-font">
+            <time>2023/7/4</time>
+            <small>公開</small>
+            <br>
+            <time>2023/7/5</time>
+            <small>0:00〜9:59間のログが無視されるバグを修正</small>
+            <br>
+            <time>2023/7/6</time>
+            <small>iOS版に対応</small>
+        </p>
     </main>
     <footer>
         <?php statisticsComponent('footer_share_nav', ['title' => $_meta->title]) ?>
@@ -60,11 +65,11 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
     <script src="/js/TalkAnalyzer_02.js"></script>
-    <script src="/js/TalkGraph_04.js"></script>
-    <script src="/js/LiveTalkAnalyzer_02.js"></script>
+    <script src="/js/TalkGraph_05.js"></script>
+    <script src="/js/LiveTalkAnalyzer_04.js"></script>
     <script>
         const liveTalkAnalyzer = new LiveTalkAnalyzer();
-        liveTalkAnalyzer.addEventListener();
+        liveTalkAnalyzer.eventListener();
     </script>
 </body>
 
