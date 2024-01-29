cd67f248eda4791e157ad48d7b4244eab76083a2
mimikyu <0203.sub@gmail.com>
2024-01-29 16:02:19 +0900

    modified:   app/Views/policy_content.php

--- app/Views/policy_content.php
+++ app/Views/policy_content.php
@@ -12,10 +12,10 @@
             <p>・オープンチャットの管理者が成長傾向を把握し、比較できる事で運営に役立つ</p>
             <p>・ユーザーがオープンチャットを見つけて参加する機会を作る</p>
             <h2>コンテンツの権利</h2>
-            <p>当サイトにおける投稿コンテンツに対して有する権利はオープンチャットの作成者及びLINE株式会社が従前どおり保持し、当サイトがかかる権利を取得することはありません。</p>
             <p>データの収集は<a href="https://openchat.line.me/robots.txt" rel="external" target="_blank">LINEオープンチャット公式サイトのrobots.txt</a><span class="line-link-icon"></span>を尊重して行います。</p>
-            <p>LINEの詳細は<a href="https://terms.line.me/line_terms?lang=ja" rel="noopener noreferrer" target="_blank">LINE利用規約</a>や<a href="https://terms.line.me/line_Square_TOU_JP?lang=ja" rel="noopener noreferrer" target="_blank">LINEオープンチャット利用規約</a>をご覧ください。</p>
             <p>公式サイトにて掲載が終了したオープンチャットは、当サイトから削除されます。</p>
+            <p>当サイトにおける投稿コンテンツに対して有する権利はオープンチャットの作成者及びLINE株式会社が従前どおり保持し、当サイトがかかる権利を取得することはありません。</p>
+            <p>LINEの詳細は<a href="https://terms.line.me/line_terms?lang=ja" rel="noopener noreferrer" target="_blank">LINE利用規約</a>や<a href="https://terms.line.me/line_Square_TOU_JP?lang=ja" rel="noopener noreferrer" target="_blank">LINEオープンチャット利用規約</a>をご覧ください。</p>
             <p>オープンチャットが当サイトに登録されないようにするには、オープンチャットの説明文に #nolog を含めます。</p>
             <p>既に登録済みのオープンチャットは、データ更新時に #nolog を検出すると削除されます。</p>
             <h2>Cookie</h2>
