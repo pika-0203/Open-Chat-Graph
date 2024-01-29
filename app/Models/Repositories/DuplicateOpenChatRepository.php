0917c454a3712d3d3a6999bc50ca89832dc19501
mimikyu <0203.sub@gmail.com>
2024-01-29 21:45:09 +0900

    modified:   app/Models/Repositories/DuplicateOpenChatRepositoryInterface.php

--- app/Models/Repositories/DuplicateOpenChatRepository.php
+++ app/Models/Repositories/DuplicateOpenChatRepository.php
@@ -4,79 +4,29 @@ declare(strict_types=1);
 
 namespace App\Models\Repositories;
 
-use App\Config\AppConfig;
 use Shadow\DB;
-use App\Services\OpenChat\Dto\OpenChatDto;
 
 class DuplicateOpenChatRepository implements DuplicateOpenChatRepositoryInterface
 {
     public function getDuplicateOpenChatInfo(): array
     {
-        $defaultImgs = implode("', '", AppConfig::DEFAULT_OPENCHAT_IMG_URL);
-
         $query =
             "SELECT
-                GROUP_CONCAT(id) as id,
-                img_url
+                GROUP_CONCAT(id) as id
             FROM
                 open_chat
-            WHERE
-                img_url NOT IN (
-                    '{$defaultImgs}',
-                    'noimage'
-                )
             GROUP BY
-                img_url
+                emid
             HAVING
                 COUNT(*) > 1";
 
         $result = DB::fetchAll($query);
 
-        foreach ($result as &$oc) {
-            $oc['id'] = array_map(fn ($id) => (int)$id, explode(',', $oc['id']));
+        $idArray = [];
+        foreach ($result as $oc) {
+            $idArray[] = array_map(fn ($id) => (int)$id, explode(',', $oc['id']));
         }
 
-        return $result;
-    }
-
-    public function findDuplicateOpenChat(OpenChatDto $dto): int|false
-    {
-        if (in_array($dto->profileImageObsHash, AppConfig::DEFAULT_OPENCHAT_IMG_URL, true)) {
-            $params = [
-                'name' => $dto->name,
-                'description' => $dto->desc,
-                'img_url' => $dto->profileImageObsHash,
-            ];
-
-            $query =
-                'SELECT
-                    id
-                FROM
-                    open_chat
-                WHERE
-                    name = BINARY :name
-                    AND description = BINARY :description
-                    AND img_url = :img_url
-                ORDER BY
-                    id ASC
-                LIMIT 1';
-        } else {
-            $params = [
-                'img_url' => $dto->profileImageObsHash,
-            ];
-
-            $query =
-                'SELECT
-                    id
-                FROM
-                    open_chat
-                WHERE
-                    img_url = :img_url
-                ORDER BY
-                    id ASC
-                LIMIT 1';
-        }
-
-        return DB::execute($query, $params)->fetchColumn();
+        return $idArray;
     }
 }
--- app/Models/Repositories/DuplicateOpenChatRepositoryInterface.php
+++ app/Models/Repositories/DuplicateOpenChatRepositoryInterface.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models\Repositories;
 
-use App\Services\OpenChat\Dto\OpenChatDto;
-
 interface DuplicateOpenChatRepositoryInterface
 {
     public function getDuplicateOpenChatInfo(): array;
-
-    public function findDuplicateOpenChat(OpenChatDto $dto): int|false;
 }
