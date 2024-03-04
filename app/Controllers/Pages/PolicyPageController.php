7df3463f5a7ce11954b3097d5d7eca1614c1eb6f
mimikyu <0203.sub@gmail.com>
2024-03-05 04:44:13 +0900

    modified:   app/Views/policy_content.php

--- app/Controllers/Pages/PolicyPageController.php
+++ app/Controllers/Pages/PolicyPageController.php
@@ -9,7 +9,7 @@ class PolicyPageController
     function index()
     {
         $_css = ['site_header', 'site_footer', 'room_list', 'terms'];
-        $_meta = meta()->setTitle('ポリシー');
+        $_meta = meta()->setTitle('オプチャグラフについて');
         return view('policy_content', compact('_meta', '_css'));
     }
 }
