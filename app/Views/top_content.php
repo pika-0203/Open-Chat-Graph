b42dab0e2f998419fb36c75fc605dd2bb819b250
mimikyu <0203.sub@gmail.com>
2024-03-21 13:19:34 +0900

    modified:   app/Views/top_content.php

--- app/Views/top_content.php
+++ app/Views/top_content.php
@@ -8,9 +8,9 @@
         <header>
             <span class="main-header-title">OPENCHAT GRAPH</span>
             <span class="main-header-title-desc">メンバー数の増加をグラフで分析</span>
-            <small><b style="color: inherit;">2024.3.21 コメント機能を実装しました</b></small>
+            <small class="top-small-desc" style="color: #ff5d6d; font-size: 13px"><b style="color: inherit;">【2024.3.21】コメント機能を実装しました</b></small>
             <br>
-            <small><a href="/policy" style="color: inherit;">オプチャグラフについてのコメント欄</a>を開設しました</small>
+            <small style="font-size: 11px; color:#777"><a href="/policy" style="font-size: inherit; color: inherit">オプチャグラフについてのコメント欄</a>を開設しました</small>
         </header>
 
         <hr>
