9cb6f97a6016d06c189caa7dac9e6a0d82950a9d
mimikyu <0203.sub@gmail.com>
2024-03-22 17:35:08 +0900

    modified:   app/Views/oc_content.php

--- public/js/site_header_footer.js
+++ public/js/site_header_footer.js
@@ -31,4 +31,51 @@ function validateStringNotEmpty(str) {
       if (!validateStringNotEmpty(e.target.elements['q'].value)) e.preventDefault()
     })
   })
+  ;(function (el) {
+    if (!el) return
+    const outer = document.getElementById('copy-btn-outer')
+    let flag = true
+
+    const copyUrl = async () => {
+      if (!flag) return
+      flag = false
+
+      const url = `https://${location.hostname}${location.pathname}`
+      const text = document.title + '\n' + url
+
+      try {
+        await navigator.clipboard.writeText(text)
+      } catch {
+        console.error('コピーできませんでした')
+        return false
+      }
+
+      document.getElementById('copy-btn-title').textContent = document.title
+      document.getElementById('copy-btn-url').textContent = url
+
+      outer.classList.remove('fade-out-copy')
+      outer.classList.add('copy-btn-copied')
+      outer.classList.add('fade-in-copy')
+
+      // 1秒後にフェードアウトアニメーションを適用し、非表示（display:none）に変更
+      setTimeout(function () {
+        outer.classList.remove('fade-in-copy')
+        outer.classList.add('fade-out-copy')
+        setTimeout(function () {
+          outer.classList.remove('copy-btn-copied')
+          flag = true
+        }, 250) // フェードアウトにかかる時間（ミリ秒）
+      }, 1500) // 表示されてからフェードアウトまでの待機時間（ミリ秒）
+
+      return true
+    }
+
+    el.addEventListener('click', () => copyUrl())
+
+    document.getElementById('copy-description').addEventListener('click', () => {
+      outer.classList.remove('fade-in')
+      outer.classList.add('fade-out')
+      outer.classList.remove('copy-btn-copied')
+    })
+  })(document.getElementById('copy-btn'))
 })()
