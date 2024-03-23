b0fe751344ed4b4ba43eecf62131a946b6b7a66b
mimikyu <0203.sub@gmail.com>
2024-03-23 16:03:32 +0900

    modified:   storage/static_data_top/real_updated_at.dat

--- tests/StaticDataGenerationTest.php
+++ tests/StaticDataGenerationTest.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Services\OpenChat;
-
-use App\Services\StaticDataGeneration;
+use App\Services\StaticData\StaticDataGenerator;
 use PHPUnit\Framework\TestCase;
 
 class StaticDataGenerationTest extends TestCase
@@ -12,22 +10,12 @@ class StaticDataGenerationTest extends TestCase
     public function test(): void
     {
         /**
-         * @var StaticDataGeneration $ssg
+         * @var StaticDataGenerator $ssg
          */
-        $ssg = app(StaticDataGeneration::class);
-        $ssg->generateLatestOpenChatStaticData();
+        $ssg = app(StaticDataGenerator::class);
+        $ssg->updateStaticData();
 
         $this->assertTrue(is_object($ssg));
     }
 
-    public function testgenerateLatestStatisticsStaticData(): void
-    {
-        /**
-         * @var StaticDataGeneration $ssg
-         */
-        $ssg = app(StaticDataGeneration::class);
-        $ssg->generateLatestStatisticsStaticData();
-
-        $this->assertTrue(is_object($ssg));
-    }
 }
