7e43ec6eabc8b2ce735d18c37dda53e546af4283
mimikyu <0203.sub@gmail.com>
2024-03-24 16:32:51 +0900

    renamed:    public/js/comment/index-D7wAf_86.js -> public/js/comment/index-DjpWYMR6.js

--- app/Models/CommentRepositories/DeleteCommentRepository.php
+++ app/Models/CommentRepositories/DeleteCommentRepository.php
@@ -6,7 +6,7 @@ namespace App\Models\CommentRepositories;
 
 class DeleteCommentRepository implements DeleteCommentRepositoryInterface
 {
-    function deleteComment(int $comment_id): void
+    function deleteComment(int $comment_id): bool
     {
         $id = compact('comment_id');
 
@@ -15,10 +15,22 @@ class DeleteCommentRepository implements DeleteCommentRepositoryInterface
             $id
         );
 
-        CommentDB::execute(
+        return CommentDB::executeAndCheckResult(
             "DELETE FROM comment WHERE comment_id = :comment_id",
             $id
         );
+    }
+
+    function deleteCommentByOcId(int $open_chat_id, int $id): bool
+    {
+        $comment_id = CommentDB::fetchColumn(
+            "SELECT comment_id FROM comment WHERE open_chat_id = :open_chat_id AND id = :id",
+            compact('open_chat_id', 'id')
+        );
+
+        if (!$comment_id) return false;
+
+        return $this->deleteComment($comment_id);
     }
 
     function deleteCommentsAll(int $open_chat_id): void
