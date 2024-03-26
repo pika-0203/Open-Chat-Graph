b626828ca6e0f175185f487fe70816324fbe48a1
mimikyu <0203.sub@gmail.com>
2024-03-26 22:21:51 +0900

    modified:   app/Views/policy_content.php

--- app/Views/policy_content.php
+++ app/Views/policy_content.php
@@ -17,13 +17,13 @@
                 <p>・ユーザーがオープンチャットを見つけて参加する機会を作る</p>
                 <h2>オープンチャットの掲載条件</h2>
                 <p>LINEオープンチャット公式サイトに掲載されているランキングを基にオープンチャットを記録します。<br>掲載が終了したオープンチャットはオプチャグラフから削除されます。</p>
-                <p style="font-size: 11px; color: #b7b7b7;">公式サイトからのデータ収集は robots.txt を尊重して行います。公式サイトでの掲載条件はオープンチャットの利用規約やLINEの情報を参照してください。</p>
+                <p style="font-size: 11px; color: #b7b7b7;">公式サイトからのデータ収集は robots.txt を尊重して行います。公式サイトでの掲載条件はオープンチャットの利用規約などLINEの情報をご確認ください。</p>
                 <p style="font-size: 11px; color: #b7b7b7;">LINEオープンチャット公式サイト: <a rel="external nofollow noopener" href="https://openchat.line.me/jp" target="_blank">https://openchat.line.me/jp</a></p>
                 <p style="font-size: 11px; color: #b7b7b7;">LINEオープンチャット利用規約: <a rel="external nofollow noopener" href="https://terms.line.me/line_Square_TOU_JP?lang=ja" target="_blank">https://terms.line.me/line_Square_TOU_JP?lang=ja</a></p>
                 <h2 style="border:none; margin-bottom:0;">コメント欄</h2>
                 <div id="comment-root" style="margin: 0;"></div>
                 <br>
-                <p style="font-size: 11px; color: #b7b7b7;">お問い合わせ窓口: <a href="mailto:support@openchat-review.me">support@openchat-review.me</a></p>
+                <p style="font-size: 11px; color: #b7b7b7;">オプチャグラフお問い合わせ窓口: <a href="mailto:support@openchat-review.me">support@openchat-review.me</a></p>
                 <p style="font-size: 11px; color: #b7b7b7;">Gihub: <a rel="external nofollow noopener" href="https://github.com/pika-0203/Open-Chat-Graph" target="_blank">https://github.com/pika-0203/Open-Chat-Graph</a></p>
             </article>
         </main>
