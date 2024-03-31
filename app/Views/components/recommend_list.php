e2e8482583dd58686adf72392e3ce735fc169d27
mimikyu <0203.sub@gmail.com>
2024-03-31 14:06:23 +0900

    modified:   public/style/room_page.css

--- app/Views/components/recommend_list.php
+++ app/Views/components/recommend_list.php
@@ -33,11 +33,35 @@
             display: none;
         }
 
+        .recommend-list a {
+            cursor: pointer;
+            text-decoration: none;
+            color: #111;
+            user-select: none;
+            display: flex;
+            flex-direction: column;
+            /* align-items: center; */
+            height: 180px;
+            justify-content: space-evenly;
+        }
+
+        .recommend-list img {
+            display: block;
+            width: 100%;
+            aspect-ratio: 1;
+            border-radius: 22.5%;
+            object-fit: cover;
+        }
+
         @media screen and (min-width: 512px) {
             .recommend-list {
                 overflow-x: unset;
                 flex-wrap: wrap;
                 justify-content: space-evenly;
+            }
+
+            .recommend-list li {
+                padding: 8px 3px;
             }
 
             .recommend-list li:nth-child(n+11) {
@@ -58,15 +82,24 @@
                 font-size: 12px;
                 cursor: pointer;
             }
+
+            .recommend-list a {
+                height: 174px;
+            }
+
+            .recommend-list img {
+                width: 100px;
+                margin: 0 auto;
+            }
         }
     </style>
     <div class="btn-wrapper">
-        <div style="display: flex; flex-direction: row; align-items: center; margin-left: 1rem;">
+        <div style="display: flex; flex-direction: row; /* align-items: center; */ margin-left: 1rem;">
             <div aria-hidden="true" style="font-size: 12px; user-select: none;">🏷️</div>
             <h3 style="all: unset;
                 font-weight: bold;
                 font-size: 13px;
-                color: #111;
+                color: #777;
             "><?php echo $category ?>の注目オプチャ</h3>
         </div>
         <button type="button" class="read-more-list-btn" onclick="this.textContent = this.parentElement.nextElementSibling.classList.toggle('show-all') ? '一部を表示' : 'もっと見る';">もっと見る</button>
@@ -74,34 +107,25 @@
     <ul class="recommend-list">
         <?php foreach ($recommend as $roc) : ?>
             <li>
-                <a style="cursor: pointer;
-                        text-decoration: none;
-                        color: #111;
-                        user-select: none;
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        height: 163px;
-                        justify-content: space-around;
-                    " href="<?php echo url('/oc/' . $roc['id']) ?>">
-                    <img style="display: block; width: 100%; aspect-ratio: 1; border-radius: 22.5%; object-fit: cover;" loading="lazy" alt="オープンチャット「<?php echo $roc['name'] ?>」のアイコン" src="<?php echo imgPreviewUrl($roc['id'], $roc['img_url']) ?>" />
+                <a href="<?php echo url('/oc/' . $roc['id']) ?>">
+                    <img loading="lazy" alt="オープンチャット「<?php echo $roc['name'] ?>」のアイコン" src="<?php echo imgPreviewUrl($roc['id'], $roc['img_url']) ?>" />
                     <h4 style=" 
                 font-size: 12px;
                 display: -webkit-box;
                 -webkit-box-orient: vertical;
-                -webkit-line-clamp: 2;
+                -webkit-line-clamp: 3;
                 overflow: hidden;
                 word-break: break-all;
                 margin: 0;
                 margin-top: 6px;
-                font-weight: normal;
-                text-align: center;
-                line-height: 120%;
+                font-weight: bold;
+                /* text-align: center; */
+                line-height: 125%;
                 overflow-wrap: anywhere;
               ">
                         <?php echo $roc['name'] ?>
                     </h4>
-                    <div style="font-size: 12px; color: #aaa; line-height: 120%; margin-top: 3px;">
+                    <div style="font-size: 12px; color: #aaa; line-height: 125%; margin-top: 3px;">
                         <span>メンバー <?php echo number_format($roc['member']) ?>人</span>
                     </div>
                 </a>
