ac3ff9a66b4bb8525e908789008e9d7d0a17530c
mimikyu <0203.sub@gmail.com>
2024-04-19 23:25:28 +0900

    modified:   app/Views/policy_content.php

--- app/Views/policy_content.php
+++ app/Views/policy_content.php
@@ -33,14 +33,14 @@
                     オープンチャットのサービス開始当初、オープンチャットを検索したり、ランキングを見る機能はLINEアプリ内限定で提供されていました。
                 </p>
                 <p>
-                    しかし、2023年10月頃に<a href="https://openchat.line.me/jp" target="_blank">「WEBブラウザ版メイン画面」（オプチャ公式サイト）</a>が公開された事により、LINEアプリ外のブラウザからオープンチャットを検索したり、ランキングを見ることが可能になりました。「WEBブラウザ版メイン画面」に掲載されているオープンチャットが、検索エンジン（Googleなど）の検索結果に表示されることで、参加経路の拡大を図るものです。
+                    しかし、2023年10月頃に<a href="https://openchat.line.me/jp" target="_blank">「WEBブラウザ版メイン画面」（オプチャ公式サイト）</a>が公開された事により、LINEアプリ外のブラウザからオープンチャットを検索したり、ランキングを見ることが可能になりました。参加経路の拡大を図るため、「WEBブラウザ版メイン画面」に掲載されているオープンチャットを検索エンジン（Googleなど）の検索結果に表示させるというものです。
                     <a href="https://ja.wikipedia.org/wiki/%E6%A4%9C%E7%B4%A2%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%B3%E6%9C%80%E9%81%A9%E5%8C%96" target="_blank">SEO</a>と言われるマーケティングの一環により、効果的に掲載を図りユーザーの認知を増やすことを目的とした媒体であると考えられます。
                 </p>
                 <p>
                 </p>
                 <p>
                     <b>オプチャグラフは「WEBブラウザ版メイン画面」の分析データを掲載し、参加経路拡大に寄与するために開発されたオープンチャット専用の検索エンジンです。</b>
-                    オプチャ公式サイトが送信しているデータを、Google・Bingなどの検索エンジンと同様の一般的なルールに基づいて公開しています。
+                    <a href="https://webtan.impress.co.jp/g/%E3%82%AF%E3%83%AD%E3%83%BC%E3%83%A9%E3%83%BC" target="_blank">クローラー</a>でオプチャ公式サイトのデータをクローリングし、Google・Bingなどの検索エンジンと同様の一般的なルールに基づいてデータを公開しています。
                 </p>
                 <p>
                     オープンチャットのデータを不正に公開するものではなく、適切な範囲内で健全な情報共有を行うWEBサイトです。
@@ -52,10 +52,8 @@
                 <p>
                     参考ページ: <a href="https://openchat-jp.line.me/other/notice_webmain_3gf87gs1" target="_blank">Webブラウザ版メイン画面公開と検索エンジン対応のお知らせ | LINEオープンチャット</a>
                 </p>
-                <p style="font-size: 12px; color: #777;">LINEオープンチャット利用規約: <a href="https://terms.line.me/line_Square_TOU_JP?lang=ja" target="_blank">https://terms.line.me/line_Square_TOU_JP?lang=ja</a>
-                    <br>
-                    データ収集は公式サイトの robots.txt を尊重して行います。
-                </p>
+                <p style="font-size: 11px; color: #777;">オプチャグラフBotのUA: Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36 (compatible; OpenChatStatsbot; +https://github.com/pika-0203/Open-Chat-Graph)<br>データ収集は公式サイトの robots.txt を尊重して行います。</p>
+                <p style="font-size: 12px; color: #777;">LINEオープンチャット利用規約: <a href="https://terms.line.me/line_Square_TOU_JP?lang=ja" target="_blank">https://terms.line.me/line_Square_TOU_JP?lang=ja</a></p>
                 <h2 style="margin-bottom: 2rem;" id="comments">オプチャグラフに関する情報共有・コメント</h2>
                 <div id="comment-root"></div>
                 <h2>お問い合わせ・ソースコード</h2>
