165cd032d8957880775362d2636a3e112ae8c2e2
mimikyu <0203.sub@gmail.com>
2024-04-22 14:46:11 +0900

    modified:   app/Views/Classes/Content/TopPageNews.php

--- app/Views/top_content.php
+++ app/Views/top_content.php
@@ -6,33 +6,6 @@
     <?php viewComponent('site_header') ?>
     <main style="margin-bottom: 0;">
         <article class="top-ranking" style="padding-top: 0; margin-top: 0; margin-bottom: 1rem">
-            <aside style="font-size: 13px; display: block;" class="unset">
-                <details style="margin:0 0 0 0; width:100%;">
-                    <summary style="color:#111; user-select:none;">
-                        <span>お知らせ</span>
-                        <span style="color: #777; font-weight:normal"><?php /** @var \App\Views\Content\TopPageNews[] $news */ echo timeElapsedString($news[0]->date->format('Y-m-d H:i:s')) ?></span>
-                    </summary>
-                    <div style="margin: .5rem 0 .5rem 0; max-height: 20rem; overflow-y: auto;">
-                        <?php foreach ($news as $el) : ?>
-                            <div style="margin-bottom: 1rem; border-bottom: 1px solid #efefef; width: 100%;">
-                                <span style="color: #111; font-size: 13px; font-weight: bold"><?php echo $el->title ?></span>
-                                <span style="color: #777; margin-left: 4px"><?php echo $el->date->format('Y/n/j G:i') ?></span>
-                                <?php foreach ($el->body as $body) : ?>
-                                    <?php if (is_array($body)) : ?>
-                                        <ul style="padding-left: 1rem;">
-                                            <?php foreach ($body as $li) : ?>
-                                                <li style="white-space: pre-line;"><?php echo $li ?></li>
-                                            <?php endforeach ?>
-                                        </ul>
-                                    <?php else : ?>
-                                        <p style="white-space: pre-line;"><?php echo $body ?></p>
-                                    <?php endif ?>
-                                <?php endforeach ?>
-                            </div>
-                        <?php endforeach ?>
-                    </div>
-                </details>
-            </aside>
             <a style="margin-bottom: 0;" class="top-ranking-readMore unset ranking-url" href="<?php echo url('ranking') ?>">
                 <span class="ranking-readMore">カテゴリーからオープンチャットを探す</span>
             </a>
@@ -169,6 +142,38 @@
                 <span class="ranking-readMore">オプチャグラフについて</span>
             </a>
         </article>
+        <aside style="font-size: 13px; display: block; margin:0;" class="unset">
+            <details style="margin:0 0 0 0; width:100%;">
+                <summary class="news-summary">
+                    <span>アップデート情報</span>
+                    <span style="color: #b7b7b7; font-weight:normal; font-size:13px"><?php /** @var \App\Views\Content\TopPageNews[] $news */ echo timeElapsedString($news[0]->date->format('Y-m-d H:i:s')) ?></span>
+                </summary>
+                <div style="position:relative;">
+                    <div style="margin: .5rem 0 .5rem 0; max-height: 20rem; overflow-y: auto;">
+                        <div style="margin-bottom: 2rem;">
+                            <?php foreach ($news as $el) : ?>
+                                <div style="margin-bottom: 1rem; border-bottom: 1px solid #efefef; width: 100%;">
+                                    <span style="color: #111; font-size: 13px; font-weight: bold"><?php echo $el->title ?></span>
+                                    <span style="color: #777; margin-left: 4px"><?php echo $el->date->format('Y/n/j G:i') ?></span>
+                                    <?php foreach ($el->body as $body) : ?>
+                                        <?php if (is_array($body)) : ?>
+                                            <ul style="padding-left: 1rem;">
+                                                <?php foreach ($body as $li) : ?>
+                                                    <li style="white-space: pre-line;"><?php echo $li ?></li>
+                                                <?php endforeach ?>
+                                            </ul>
+                                        <?php else : ?>
+                                            <p style="white-space: pre-line;"><?php echo $body ?></p>
+                                        <?php endif ?>
+                                    <?php endforeach ?>
+                                </div>
+                            <?php endforeach ?>
+                        </div>
+                    </div>
+                    <div class="gradient-bottom"></div>
+                </div>
+            </details>
+        </aside>
     </main>
     <footer>
         <?php viewComponent('footer_share_nav', ['title' => $_meta->title]) ?>
