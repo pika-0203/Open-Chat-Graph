da3d10f58939269c07961f658432c8fb9b59d14b
mimikyu <0203.sub@gmail.com>
2024-04-25 07:23:11 +0900

    modified:   public/style/room_list.css

--- app/Views/components/open_chat_list_ranking_comment.php
+++ app/Views/components/open_chat_list_ranking_comment.php
@@ -1,30 +1,69 @@
 <!-- @param array $openChatList -->
 <!-- @param bool $isHourly -->
 <ol class="openchat-item-list unset">
+  <?php $count = 0; ?>
   <?php foreach ($openChatList as $oc) : ?>
-    <li class="openchat-item unset">
-      <a class="link-overlay unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>" tabindex="-1" aria-hidden="true">
-        <span class="visually-hidden"><?php echo $oc['name'] ?></span>
-      </a>
-      <img alt="<?php echo $oc['name'] ?>" class="openchat-item-img" loading="lazy" src="<?php echo imgPreviewUrl($oc['id'], $oc['img_url']) ?>">
-      <h3 class="unset">
-        <a class="openchat-item-title unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>">
-          <div class="comment-user">
-            <span><?php echo $oc['user'] ?></span>
-          </div>
-          <div class="comment-name">
-            <span aria-hidden="true">@</span><?php if (($oc['emblem'] ?? 0) === 1) : ?><span class="super-icon sp"></span><?php elseif (($oc['emblem'] ?? 0) === 2) : ?><span class="super-icon official"></span><?php endif ?><span><?php echo $oc['name'] ?></span>
-          </div>
-          <div class="comment-time">
-            <span><?php echo timeElapsedString($oc['time']) ?></span>
-          </div>
+    <?php if ($count > 5) continue; ?>
+    <?php if (mb_strlen($oc['description']) > 10) : ?>
+      <?php $count++; ?>
+      <li class="openchat-item unset">
+        <a class="link-overlay unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>" tabindex="-1" aria-hidden="true">
+          <span class="visually-hidden"><?php echo $oc['name'] ?></span>
         </a>
-      </h3>
-      <?php if ($oc['description']) : ?>
+        <img alt="<?php echo $oc['name'] ?>" class="openchat-item-img" loading="lazy" src="<?php echo imgPreviewUrl($oc['id'], $oc['img_url']) ?>">
+        <h3 class="unset">
+          <a class="openchat-item-title unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>">
+            <div class="comment-user">
+              <span><?php echo $oc['user'] ?></span>
+            </div>
+            <div class="comment-name">
+              <span aria-hidden="true">@</span><?php if (($oc['emblem'] ?? 0) === 1) : ?><span class="super-icon sp"></span><?php elseif (($oc['emblem'] ?? 0) === 2) : ?><span class="super-icon official"></span><?php endif ?><span><?php echo $oc['name'] ?></span>
+            </div>
+            <div class="comment-time">
+              <span><?php echo timeElapsedString($oc['time']) ?></span>
+            </div>
+          </a>
+        </h3>
         <p class="openchat-item-desc unset"><?php echo $oc['description'] ?></p>
-      <?php else : ?>
-        <p class="openchat-item-desc unset" style="color: #b7b7b7;">削除されたコメント</p>
-      <?php endif ?>
-    </li>
+      </li>
+    <?php elseif ((mb_strlen($oc['description'] > 0))) : ?>
+      <li class="unset" style="position: relative; display:block; padding: 8px 0;">
+        <a class="link-overlay unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>" tabindex="-1" aria-hidden="true">
+          <span class="visually-hidden"><?php echo $oc['name'] ?></span>
+        </a>
+        <h3 class="unset">
+          <a style="font-size: 11px;" class="openchat-item-title unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>">
+            <div class="comment-user">
+              <span><?php echo mb_strlen($oc['description']) ?>文字のコメント</span>
+            </div>
+            <div class="comment-name">
+              <span aria-hidden="true">@</span><?php if (($oc['emblem'] ?? 0) === 1) : ?><span class="super-icon sp"></span><?php elseif (($oc['emblem'] ?? 0) === 2) : ?><span class="super-icon official"></span><?php endif ?><span><?php echo $oc['name'] ?></span>
+            </div>
+            <div class="comment-time">
+              <span><?php echo timeElapsedString($oc['time']) ?></span>
+            </div>
+          </a>
+        </h3>
+      </li>
+    <?php else : ?>
+      <li class="unset" style="position: relative; display:block; padding: 8px 0;">
+        <a class="link-overlay unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>" tabindex="-1" aria-hidden="true">
+          <span class="visually-hidden"><?php echo $oc['name'] ?></span>
+        </a>
+        <h3 class="unset">
+          <a style="font-size: 11px;" class="openchat-item-title unset" href="<?php echo  $oc['id'] ? url('/oc/' . $oc['id']) : url('policy#comments') ?>">
+            <div class="comment-user">
+              <span>削除されたコメント</span>
+            </div>
+            <div class="comment-name">
+              <span aria-hidden="true">@</span><?php if (($oc['emblem'] ?? 0) === 1) : ?><span class="super-icon sp"></span><?php elseif (($oc['emblem'] ?? 0) === 2) : ?><span class="super-icon official"></span><?php endif ?><span><?php echo $oc['name'] ?></span>
+            </div>
+            <div class="comment-time">
+              <span><?php echo timeElapsedString($oc['time']) ?></span>
+            </div>
+          </a>
+        </h3>
+      </li>
+    <?php endif ?>
   <?php endforeach ?>
 </ol>
