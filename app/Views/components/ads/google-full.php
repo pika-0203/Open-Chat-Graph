a0b644a177adb39c20b84815a1f7012f94501418
mimikyu <0203.sub@gmail.com>
2024-05-10 20:00:20 +0900

    modified:   app/Views/components/ads/google-full.php

--- app/Views/components/ads/google-full.php
+++ app/Views/components/ads/google-full.php
@@ -1,6 +1,5 @@
 <!-- OCページ -->
 <div style="position: relative;">
-     <span aria-hidden="true" style="position: absolute; z-index: -1; font-size: 25px; padding: 1rem; color: #ff5d6d; font-weight: bold;">アドブロックの設定を解除してください。</span>
      <ins class="adsbygoogle rectangle-ads" style="display:block; background: rgb(250, 250, 250);" data-ad-client="ca-pub-2330982526015125" data-ad-slot="8037531176"></ins>
      <script>
           (adsbygoogle = window.adsbygoogle || []).push({});
