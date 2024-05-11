05e7decb7e3fb954f9ab07b5745052e274025db4
mimikyu <0203.sub@gmail.com>
2024-05-11 16:32:07 +0900

    modified:   app/Views/top_content.php

--- app/Views/top_content.php
+++ app/Views/top_content.php
@@ -88,6 +88,9 @@ viewComponent('head', compact('_css', '_meta', '_schema')) ?>
 
         <?php if ($myList) : ?>
             <article class="mylist">
+                <div style="margin: 1rem 0;">
+                    <?php viewComponent('ads/google-horizontal') ?>
+                </div>
                 <div class="refresh-time openchat-list-date">
                     <span style="font-weight: bold; color:#111; font-size:13px; margin: 0; line-height: unset;">ピン留め (24時間の人数増加)</span>
                     <span style="font-weight: normal; color:#aaa; font-size:13px; margin: 0; line-height: unset;"><?php echo $dto->hourlyUpdatedAt->format('G:i') ?></span>
