9470bca1060f1bda9d09da3ff47817da4a3d5fd1
mimikyu <0203.sub@gmail.com>
2024-05-15 12:21:51 +0900

    modified:   app/Views/components/policy_head.php

--- app/Views/errors/oc_error.php
+++ app/Views/errors/oc_error.php
@@ -16,8 +16,8 @@
         <link rel="stylesheet" href="<?php echo fileUrl("style/{$css}.css") ?>">
     <?php endforeach ?>
     <link rel="icon" type="image/png" href="<?php echo url(\App\Config\AppConfig::SITE_ICON_FILE_PATH) ?>">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2330982526015125" data-overlays="bottom" crossorigin="anonymous"></script>
-</head>
+<!--     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2330982526015125" data-overlays="bottom" crossorigin="anonymous"></script>
+ --></head>
 
 <body class="body">
     <style>
