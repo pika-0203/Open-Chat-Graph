8369d9fb4860f6056e17bbb5f085b2e7ed835dd5
mimikyu <0203.sub@gmail.com>
2024-06-19 09:17:21 +0900

    modified:   app/Views/accreditation/login.php

--- app/Views/accreditation/login.php
+++ app/Views/accreditation/login.php
@@ -14,28 +14,40 @@ $returnTo = "?return_to=/accreditation/{$view->controller->type->value}";
     <?php $view->header() ?>
     <main>
         <section>
-            <a style="display:flex; width:fit-content" href="/auth/login<?php echo $returnTo ?>/home"><img style="disply: block; width: 240px;" src="<?php echo fileUrl('assets/line_login_btn.png') ?>"></a>
-            <hr>
-            <aside>
-                <b>iOS・Androidの推奨ブラウザ</b>
-                <ul>
-                    <li>
-                        デフォルトに設定されているブラウザ
-                    </li>
-                    <li>
-                        LINEアプリ内ブラウザ
-                    </li>
-                </ul>
-            </aside>
+            <div>
+                <div>
+                    <a style="display:flex; width:fit-content" href="/auth/login<?php echo $returnTo ?>/home"><img style="disply: block; width: 240px;" src="<?php echo fileUrl('assets/line_login_btn.png') ?>"></a>
+                </div>
+            </div>
             <hr>
             <p>
-                当サイト内ではユーザー登録のためにLINEログインが必要です。<br>
-                <br>
-                当サイトではLINEの個人名や個人情報、LINEに関する利用者の情報を取得しないため、安心してご利用いただけます。<br>
-                LINEアプリに当サイトから通知が来ることもありません。<br>
+                <small>
+                    当サイト内ではユーザー登録のためにLINEログインが必要です。<br>
+                    <br>
+                    当サイトではLINEの個人名や個人情報、LINEに関する利用者の情報を取得しないため、安心してご利用いただけます。<br>
+                    LINEアプリに当サイトから通知が来ることもありません。<br>
+                </small>
             </p>
             <hr>
             <?php $view->term() ?>
+            <hr>
+            <aside>
+                <small>
+                    <b>iOS・Androidの推奨ブラウザ</b>
+                </small>
+                <ul style="margin: 0;">
+                    <li>
+                        <small>
+                            デフォルトに設定されているブラウザ
+                        </small>
+                    </li>
+                    <li>
+                        <small>
+                            LINEアプリ内ブラウザ
+                        </small>
+                    </li>
+                </ul>
+            </aside>
         </section>
     </main>
     <?php $view->footer() ?>
