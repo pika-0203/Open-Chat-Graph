2ca59f8c4827eb7f53153812778959424a1a3bc8
mimikyu <0203.sub@gmail.com>
2024-06-29 18:12:16 +0900

    modified:   app/Views/components/recommend_list2_accreditation.php

--- app/Views/components/recommend_list2_accreditation.php
+++ app/Views/components/recommend_list2_accreditation.php
@@ -1,8 +1,8 @@
-<article class="top-ranking not-rank" style="padding-top: 0;">
+<article class="top-ranking not-rank" style="padding: 0 0 1rem 0;">
     <header class="openchat-list-title-area unset">
         <div class="openchat-list-date unset ranking-url">
             <h2 class="unset">
-                <span style="line-height: 1.5; font-size: 14px; color: #111; font-weight: bold;">オプチャのクイズ</span>
+                <span style="line-height: 1.5; font-size: 15px; color: #111; font-weight: bold;">オプチャのクイズ</span>
             </h2>
         </div>
     </header>
@@ -14,7 +14,7 @@
             <li class="openchat-item unset" style="padding-left: 0; min-height: 0; cursor: unset; margin: 6px 1.5rem 6px 0">
                 <div style="display: flex; flex-direction: row; flex-wrap:nowrap;">
                     <span style="user-select: none; min-width:20px; display:block; color: #777;" aria-hidden="true">・</span>
-                    <a href="<?php echo url('accreditation?id=' . $question->id) ?>" class="openchat-item-desc" style=" color: rgb(39, 85, 172); font-size: 13px; font-weight: bold; letter-spacing: -0.3px; -webkit-line-clamp: 2; text-decoration: unset; line-height: 1.3;"><?php echo $question->question ?></a>
+                    <a href="<?php echo url('accreditation?id=' . $question->id) ?>" class="openchat-item-desc" style=" color: rgb(39, 85, 172); font-size: 15px; font-weight: bold; letter-spacing: -0.3px; -webkit-line-clamp: 2; text-decoration: unset; line-height: 1.25;"><?php echo $question->question ?></a>
                 </div>
             </li>
         <?php endforeach ?>
