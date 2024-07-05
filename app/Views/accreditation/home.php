b14f0c9e656512b8a181f365eaf333032a04a1de
mimikyu <0203.sub@gmail.com>
2024-07-06 08:46:28 +0900

    modified:   app/Views/accreditation/home.php

--- app/Views/accreditation/home.php
+++ app/Views/accreditation/home.php
@@ -199,8 +199,8 @@ $view = new AccreditationAdminViewContent($controller);
         </div>
         <br>
 
-        <details>
-            <summary style="width: fit-content; font-size: 13px;">オプチャ検定のPV数を見る</summary>
+        <details style="margin-top: 10px;">
+            <summary style="width: fit-content; font-size: 15px;">オプチャ検定のPV数を見る</summary>
             <br>
             <div class="graph-ifame">
                 <iframe loading=lazy src="https://lookerstudio.google.com/embed/reporting/12373f52-b8f2-42d1-9935-d2ffd6bebfa2/page/usU4D" frameborder="0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
