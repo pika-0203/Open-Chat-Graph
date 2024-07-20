31e2c87e381e06a6cd07f9422540d73594e4a21f
mimikyu <0203.sub@gmail.com>
2024-07-20 21:46:35 +0900

    deleted:    app/Controllers/Pages/AccreditationController.php

--- app/Config/AppConfig.php
+++ app/Config/AppConfig.php
@@ -111,6 +111,4 @@ class AppConfig
 
     const OPEN_CHAT_ID_DATA_FILE_PATH = __DIR__ . '/../../storage/OpenChatBackupData';
     const COMMENT_DATA_FILE_PATH = __DIR__ . '/../../storage/CommentBackupData';
-
-    const ACCREDITATION_DATA_FILE_PATH = __DIR__ . '/../../storage/AccreditationBackupData';
 }
