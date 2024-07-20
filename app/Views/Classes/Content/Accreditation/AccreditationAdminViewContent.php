0067b54cadb65dde357dffce9b30efc2c0597cad
mimikyu <0203.sub@gmail.com>
2024-07-20 10:32:02 +0900

    modified:   app/Views/Classes/Content/Accreditation/AccreditationAdminViewContent.php

--- app/Views/Classes/Content/Accreditation/AccreditationAdminViewContent.php
+++ app/Views/Classes/Content/Accreditation/AccreditationAdminViewContent.php
@@ -642,7 +642,7 @@ class AccreditationAdminViewContent
             <?php if (in_array($this->controller->pageType, ['login', 'home'])) : ?>
                 <hr style="margin-top: 0rem;">
                 <p>
-                    <small>このサイトはLINEオープンチャット非公式です。LINEヤフー社はこの内容に関与していません。</small>
+                    <small>このサイトはLINEオープンチャット非公式のユーザーコミュニティです。</small>
                 </p>
             <?php elseif ($scrollBtn) : ?>
                 <small style="text-decoration: underline; color: #111; cursor:pointer; display:block; margin-left: auto; width:fit-content;" onclick="window.scroll({top: 0,behavior:'smooth'})">↑ 一番上までスクロール</small>
