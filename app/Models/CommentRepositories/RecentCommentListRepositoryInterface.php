04d692685d31e8853feb6081378fbe88b4c125d4
mimimiku778 <0203.sub@gmail.com>
2024-10-17 02:15:00 +0900

    modified:   app/Models/CommentRepositories/RecentCommentListRepository.php

--- app/Models/CommentRepositories/RecentCommentListRepositoryInterface.php
+++ app/Models/CommentRepositories/RecentCommentListRepositoryInterface.php
@@ -7,5 +7,18 @@ interface RecentCommentListRepositoryInterface
     /**
      * @return array{ id:int,user:string,name:string,img_url:string,description:string,member:int,emblem:int,category:int,time:string }[]
      */
-    public function findRecentCommentOpenChatAll(int $offset, int $limit, string $adminId = '', string $user_id = '', int $open_chat_id = 0): array;
+    public function findRecentCommentOpenChatAll(
+        int $offset,
+        int $limit,
+        string $adminId = '',
+        string $user_id = '',
+        int $open_chat_id = 0,
+        string $order = 'DESC',
+    ): array;
+
+    public function getRecordCount(
+        string $adminId = '',
+        string $user_id = '',
+        int $open_chat_id = 0,
+    ): int;
 }
