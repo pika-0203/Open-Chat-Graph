4cd33a67798efb96a32720f19de576e7302d6dc2
mimimiku778 <0203.sub@gmail.com>
2024-10-17 02:09:41 +0900

    new file:   app/Views/Classes/OpenChatPagination.php

--- app/Views/Classes/OpenChatStatisticsRecent.php
+++ app/Views/Classes/OpenChatStatisticsRecent.php
@@ -6,62 +6,28 @@ namespace App\Views;
 
 use App\Models\Repositories\OpenChatListRepositoryInterface;
 use App\Services\Traits\TraitPaginationRecordsCalculator;
-use App\Config\AppConfig;
 
 class OpenChatStatisticsRecent
 {
     use TraitPaginationRecordsCalculator;
 
     public function __construct(
-        private OpenChatListRepositoryInterface $openChatListRepository
-    ) {
-    }
-
+        private OpenChatListRepositoryInterface $openChatListRepository,
+        private OpenChatPagination $openChatPagination,
+    ) {}
+        
     /**
      * @return array|false `['pageNumber' => int, 'maxPageNumber' => int, 'openChatList' => array, 'totalRecords' => int, 'labelArray' => array]`
      */
     public function getAllOrderByRegistrationDate(int $pageNumber): array|false
     {
         $labelArray = $this->openChatListRepository->findAllOrderByIdCreatedAtColumn();
-
-        return $this->get(
+        
+        return $this->openChatPagination->getSelectElementArgOrderDesc(
             $pageNumber,
             count($labelArray),
             $this->openChatListRepository->findAllOrderById(...),
             $labelArray
-        );
-    }
-
-    /**
-     * @return array|false `['pageNumber' => int, 'maxPageNumber' => int, 'openChatList' => array, 'totalRecords' => int, 'labelArray' => array]`
-     */
-    private function get(int $pageNumber, int $totalRecords, \Closure $repository, array $labelArray = []): array|false
-    {
-        $limit = AppConfig::OPEN_CHAT_LIST_LIMIT;
-
-        // ページの最大数を取得する
-        $maxPageNumber = $this->calcMaxPages($totalRecords, $limit);
-
-        if(!$pageNumber) {
-            $pageNumber = $maxPageNumber;
-        }
-
-        if ($pageNumber > $maxPageNumber) {
-            // 現在のページ番号が最大ページ番号を超えている場合
-            return false;
-        }
-
-        $repoArgs = [$this->calcOffset($pageNumber, $limit), $limit * $pageNumber];
-
-        // リストを取得する
-        $openChatList = array_reverse($repository(...$repoArgs));
-
-        return compact(
-            'pageNumber',
-            'maxPageNumber',
-            'openChatList',
-            'totalRecords',
-            'labelArray',
         );
     }
 }
