dea178f95bdea48d6db14902230ca210a0eebe4b
mimimiku778 <0203.sub@gmail.com>
2024-12-01 13:34:58 +0900

    oc_contentのタグのリンク先をランキングSPAからおすすめページに変更

--- app/Views/oc_content.php
+++ app/Views/oc_content.php
@@ -145,7 +145,7 @@ viewComponent('oc_head', compact('_css', '_meta', '_schema', '_chartArgDto', '_s
               <a href="<?php echo url('ranking' . ($oc['category'] ? ('/' . $oc['category']) : '')) ?>" style="width:fit-content; color:inherit; text-wrap: wrap;"><?php echo $category ?></a>
             <?php endif ?>
             <?php if (isset($recommend[2]) && $recommend[2]) : ?>
-              <a href="<?php echo url('ranking?keyword=' . urlencode('tag:' . htmlspecialchars_decode($recommend[2]))) ?>" style="width:fit-content; color:inherit; text-wrap: wrap;"><?php echo $recommend[2] ?></a>
+              <a href="<?php echo url('recommend?tag=' . urlencode(htmlspecialchars_decode($recommend[2]))) ?>" style="width:fit-content; color:inherit; text-wrap: wrap;"><?php echo $recommend[2] ?></a>
             <?php endif ?>
           </div>
         </span>
