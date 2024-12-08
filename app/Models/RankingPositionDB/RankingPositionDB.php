49336e1da4003dddd283794fe6800b9ed79a1086
mimimiku778 <0203.sub@gmail.com>
2024-12-08 20:55:36 +0900

    RankingPositionDBの接続先参照クラス名を変更

--- app/Models/RankingPositionDB/RankingPositionDB.php
+++ app/Models/RankingPositionDB/RankingPositionDB.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models\RankingPositionDB;
 
-use App\Config\RankingPositionDBConfig;
+use App\Config\AppConfig;
 use Shadow\DBInterface;
 use Shadow\DB;
 
@@ -12,8 +12,8 @@ class RankingPositionDB extends DB implements DBInterface
 {
     public static ?\PDO $pdo = null;
 
-    public static function connect(string $class = RankingPositionDBConfig::class): \PDO
+    public static function connect(string $class = ''): \PDO
     {
-        return parent::connect($class);
+        return parent::connect($class ?: AppConfig::$RankingPositionDBConfigClass);
     }
 }
