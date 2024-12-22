ba096cf9534d125380feb9dccac260ff7c50ea4c
mimimiku778 <0203.sub@gmail.com>
2024-12-23 00:16:23 +0900

    new file:   cron_half_check_provisional.php

---
+++ cron_half_check_provisional.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($argv[1]) && $argv[1]) {
+    define('URL_ROOT', $argv[1]);
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Services\Cron\Provisional\SyncOpenChat;
+use App\Services\Admin\AdminTool;
+
+/**
+ * @var SyncOpenChat $syncOpenChat
+ */
+$syncOpenChat = app(SyncOpenChat::class);
+try {
+    $syncOpenChat->handleHalfHourCheck();
+} catch (\Throwable $e) {
+    addCronLog($e->__toString());
+    AdminTool::sendLineNofity($e->__toString());
+}
