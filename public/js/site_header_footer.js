68b29f8aabdf8aba57f4a5900c876aa8da8f7d7c
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-06 07:48:43 +0900

    'ocgraph.wpx.jp'でのアドブロック対策を無効化

--- public/js/site_header_footer.js
+++ public/js/site_header_footer.js
@@ -253,4 +253,4 @@ async function blockblock() {
     })
 }
 
-if(typeof admin === 'undefined' || !admin) blockblock()
+if(typeof admin === 'undefined' || !admin || location.host !== 'ocgraph.wpx.jp') blockblock()
