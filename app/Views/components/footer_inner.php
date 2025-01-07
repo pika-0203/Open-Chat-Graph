8eb255d56b40b6f33d5cc7aa7f74a38d1250f90e
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-08 07:26:47 +0900

    翻訳を追加

--- app/Views/components/footer_inner.php
+++ app/Views/components/footer_inner.php
@@ -7,18 +7,17 @@
         </ul>
         <ul class="footer-link-inner">
             <li><a class="unset" href="<?php echo url('policy/privacy') ?>">プライバシーポリシー</a></il>
-            <li><a class="unset" style="margin-bottom: 0;" href="<?php echo url('policy/term') ?>">利用規約</a></il>
-            <!-- <li><a class="unset" href="<?php echo url('ranking') ?>">カテゴリから探す</a></li> -->
-            <!-- <li><a class="unset" href="<?php echo url('labs') ?>">分析Labs</a></li> -->
+            <li><a class="unset" href="<?php echo url('policy/term') ?>">利用規約</a></il>
+            <li><a class="unset" style="margin-bottom: 0;" href="<?php echo t('https://openchat.line.me/jp') ?>" target="_blank"><?php echo t('【公式】LINEオープンチャット') ?><span class="line-link-icon777"></span></a></li>
         </ul>
     </section>
-    <!-- <aside class="open-btn2">
-        <a href="https://openchat-jp.line.me/other/beginners_guide" class="app_link app-dl">
-            <span class="text">はじめてのLINEオープンチャットガイド（LINE公式）</span>
+    <aside class="open-btn2">
+        <a href="<?php echo t('https://openchat-jp.line.me/other/beginners_guide') ?>" class="app_link app-dl">
+            <span class="text"><?php echo t('はじめてのLINEオープンチャットガイド（LINE公式）') ?></span>
         </a>
         <a href="https://line.me/download" class="app_link app-dl">
-            <span class="text">LINEアプリをダウンロード（LINE公式）</span>
+            <span class="text"><?php echo t('LINEアプリをダウンロード（LINE公式）') ?></span>
         </a>
-    </aside> -->
-    <div class="copyright">オプチャグラフ<span><a class="unset" style="text-decoration: underline; cursor: pointer;" href="https://github.com/pika-0203" target="_blank">Project on GitHub @pika-0203</a></span></div>
+    </aside>
+    <div class="copyright"><?php echo t('オプチャグラフ') ?><span><a class="unset" style="text-decoration: underline; cursor: pointer;" href="https://github.com/pika-0203" target="_blank">Project on GitHub @pika-0203</a></span></div>
 </nav>
