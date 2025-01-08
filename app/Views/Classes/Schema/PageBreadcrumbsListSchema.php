4beb9ecd056e24a68442c7025b50a0e1801ac2a4
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-08 09:11:28 +0900

    url()の修正

--- app/Views/Classes/Schema/PageBreadcrumbsListSchema.php
+++ app/Views/Classes/Schema/PageBreadcrumbsListSchema.php
@@ -32,7 +32,7 @@ class PageBreadcrumbsListSchema
             Schema::listItem()
                 ->position(1)
                 ->name('トップ')
-                ->item(rtrim(url(), '/')),
+                ->item(url('/')),
             Schema::listItem()
                 ->position(2)
                 ->name($listItemName)
@@ -61,8 +61,8 @@ class PageBreadcrumbsListSchema
             ->logo($publisherLogo)
             ->description($this->metadata->description)
             ->email('support@openchat-review.me')
-            ->url(url())
-            ->sameAs(url('policy'));
+            ->url(AppConfig::$siteUrl)
+            ->sameAs(AppConfig::$siteUrl . '/policy');
     }
 
     function person()
