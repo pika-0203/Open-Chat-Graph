7f0348b28e90797c57487b3405225c27acedee9a
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-14 08:17:04 +0900

    modified:   app/Views/top_content.php

--- app/Views/top_content.php
+++ app/Views/top_content.php
@@ -14,7 +14,9 @@ viewComponent('head', compact('_css', '_meta', '_schema')) ?>
     <div class="pad-side-top-ranking body" style="overflow: hidden; padding-top: 0px;">
         <hr class="hr-top">
         <?php viewComponent('topic_tag', ['topPageDto' => $dto]) ?>
-
+        <hr class="hr-bottom">
+        <?php GAd::output(GAd::AD_SLOTS['ocTopWide2']) ?>
+        <hr class="hr-top">
         <?php if ($dto->recentCommentList): ?>
             <div id="myListDiv" style="transition: all 0.3s; opacity: 0;"></div>
             <?php viewComponent('top_ranking_recent_comments', ['recentCommentList' => $dto->recentCommentList]) ?>
