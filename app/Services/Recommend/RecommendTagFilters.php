c36aafbda8347535233ec5d39ab5b1a10032e87c
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-18 17:17:46 +0900

    modified:   app/Services/Recommend/RecommendTagFilters.php

--- app/Services/Recommend/RecommendTagFilters.php
+++ app/Services/Recommend/RecommendTagFilters.php
@@ -7,8 +7,8 @@ namespace App\Services\Recommend;
 class RecommendTagFilters
 {
     const RecommendPageTagFilter = [
-        "unistyle",
-        "jobhunt",
+        "unistyle", // TODO: 削除済み
+        "jobhunt", // TODO: 削除済み
     ];
 
     const FilteredTagSort = [
