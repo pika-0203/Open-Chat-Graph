75f40a45f42575ffa424d9b1272d260364908a2a
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-21 22:41:07 +0900

    modified:   app/Views/components/not_found_list.php

--- app/Views/components/not_found_list.php
+++ app/Views/components/not_found_list.php
@@ -4,19 +4,11 @@
     use App\Views\Ads\GoogleAdsence as GAd; ?>
 
     <?php viewComponent('top_ranking_comment_list_hour', compact('dto')) ?>
-    <hr class="hr-bottom">
-    <?php GAd::output(GAd::AD_SLOTS['siteSeparatorRectangle']) ?>
-    <hr class="hr-top">
+
     <?php viewComponent('top_ranking_comment_list_hour24', compact('dto')) ?>
-    <hr class="hr-bottom">
-    <?php GAd::output(GAd::AD_SLOTS['siteSeparatorRectangle']) ?>
-    <hr class="hr-top">
+
     <?php viewComponent('top_ranking_comment_list_member', compact('dto')) ?>
-    <hr class="hr-bottom">
-    <?php GAd::output(GAd::AD_SLOTS['siteSeparatorRectangle']) ?>
-    <hr class="hr-top">
+
     <?php viewComponent('top_ranking_comment_list_week', compact('dto')) ?>
-    <hr class="hr-bottom">
-    <?php GAd::output(GAd::AD_SLOTS['siteSeparatorRectangle']) ?>
-    <?php GAd::loadAdsTag() ?>
+
 </main>
