797593aa734bdaf32e89bcb9543ce1ffe2d77a5e
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-01-27 23:31:24 +0900

    modified:   .devcontainer/devcontainer.json

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,3 +1,8 @@
-sed -i 's/host\.docker\.internal/172.17.0.1/g' /var/www/html/docker/app/php.ini
+cat << 'EOF' > /var/www/html/.user.ini
+xdebug.client_host=172.17.0.1
+EOF
+
 cd /var/www/html
 composer install
+
+service apache2 reload
