df06e8bf7c6004f1bb95e18d75fa489ac674cecc
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-02-02 13:04:59 +0900

    modified:   app/Views/recommend_content.php

--- app/Views/oc_content.php
+++ app/Views/oc_content.php
@@ -274,8 +274,6 @@ viewComponent('oc_head', compact('_css', '_meta', '_schema', '_chartArgDto', '_s
       <?php viewComponent('topic_tag', compact('topPageDto')) ?>
     </aside>
 
-    <?php GAd::output(GAd::AD_SLOTS['ocTopHorizontal']) ?>
-
     <aside class="recommend-list-aside">
       <?php viewComponent('top_ranking_comment_list_hour', ['dto' => $topPageDto]) ?>
     </aside>
--- app/Views/recommend_content.php
+++ app/Views/recommend_content.php
@@ -132,20 +132,18 @@ viewComponent('head', compact('_css', '_schema', 'canonical') + ['_meta' => $_me
         </section>
       <?php endif ?>
 
-      <?php GAd::output(GAd::AD_SLOTS['recommendTopHorizontal']) ?>
       <aside class="list-aside recommend-ranking-bottom" style="padding-top: 0; margin-bottom: 0;">
         <?php if (isset($recommend)) : ?>
           <?php viewComponent('recommend_content_tags', ['tags' => $recommend->getFilterdTags(false, null), 'tag' => $tag]) ?>
         <?php endif ?>
       </aside>
     </section>
+    <?php GAd::output(GAd::AD_SLOTS['recommendTopHorizontal']) ?>
 
     <aside class="top-ranking-list-aside">
       <?php viewComponent('topic_tag', compact('topPageDto')) ?>
     </aside>
 
-    <?php GAd::output(GAd::AD_SLOTS['recommendSeparatorWide'])
-    ?>
     <aside class="top-ranking-list-aside">
       <?php viewComponent('top_ranking_comment_list_hour', ['dto' => $topPageDto]) ?>
     </aside>
