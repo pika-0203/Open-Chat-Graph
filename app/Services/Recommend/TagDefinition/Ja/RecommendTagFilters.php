c0c5477e5cfe86b61effc3467be9f50ed74ff845
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-02-02 18:01:30 +0900

    modify tag pokepoke

--- app/Services/Recommend/TagDefinition/Ja/RecommendTagFilters.php
+++ app/Services/Recommend/TagDefinition/Ja/RecommendTagFilters.php
@@ -127,5 +127,6 @@ class RecommendTagFilters
         'クーポン・お得情報' => 'クーポン・無料配布',
         'ロック' => '邦ロック',
         '整形' => '美容整形',
+        'ポケポケ（Pokémon TCG Pocket／ポケモンカード アプリ）' => 'ポケポケ（Pokémon TCG Pocket／PTCGP）',
     ];
 }
