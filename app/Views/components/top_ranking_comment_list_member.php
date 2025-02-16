59417580926f1da10074950bb5b272543a03fed5
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-02-16 21:31:44 +0900

    ランキングリストの取得制限を静的プロパティを使用して適用し、各ランキングコンポーネントでの表示を更新しました

--- app/Views/components/top_ranking_comment_list_member.php
+++ app/Views/components/top_ranking_comment_list_member.php
@@ -6,7 +6,7 @@
             </h2>
         </div>
     </header>
-    <?php viewComponent('open_chat_list_ranking', ['openChatList' => $dto->popularList, 'noReverse' => true, 'showReverseListMedal' => true]) ?>
+    <?php viewComponent('open_chat_list_ranking', ['openChatList' => array_slice($dto->popularList, 0, \App\Config\AppConfig::$listLimitTopRanking), 'noReverse' => true, 'showReverseListMedal' => true]) ?>
     <a class="top-ranking-readMore unset ranking-url white-btn" href="<?php echo url('ranking') ?>">
         <span class="ranking-readMore"><?php echo t('人数ランキングをもっと見る') ?></span>
     </a>
