c8adb2f94680cea4697bed5d5174bae612e2a0da
Mimikyu <116529486+mimimiku778@users.noreply.github.com>
2025-03-21 02:15:34 +0900

    DB接続の再接続処理を強化し、2006例外時に再試行するロジックを追加

--- app/Models/Repositories/DB.php
+++ app/Models/Repositories/DB.php
@@ -11,6 +11,7 @@ use Shared\MimimalCmsConfig;
 class DB extends \Shadow\DB implements DBInterface
 {
     public static ?\PDO $pdo = null;
+    private const GET_DAILY_POSITION_USLEEP_TIME = 1000000; // 1 seconds
 
     public static function connect(?array $config = null): \PDO
     {
@@ -25,10 +26,21 @@ class DB extends \Shadow\DB implements DBInterface
             return parent::execute($query, $params);
         } catch (\PDOException $e) {
             if ($e->errorInfo[1] === 2006) {
-                DB::$pdo = null;
-                return parent::execute($query, $params);
+                self::$pdo = null;
+
+                try {
+                    return parent::execute($query, $params);
+                } catch (\PDOException $e) {
+                    if ($e->errorInfo[1] === 2006) {
+                        self::$pdo = null;
+                        usleep(self::GET_DAILY_POSITION_USLEEP_TIME);
+                        return parent::execute($query, $params);
+                    }
+
+                    throw $e;
+                }
             }
-            
+
             throw $e;
         }
     }
