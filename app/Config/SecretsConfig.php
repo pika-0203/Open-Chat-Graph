c81c567dead28317a074f069218181fe96e38f47
mimimiku778 <0203.sub@gmail.com>
2025-06-24 22:34:03 +0900

    Refactor code structure for improved readability and maintainability

--- app/Config/SecretsConfig.php
+++ app/Config/SecretsConfig.php
@@ -11,4 +11,5 @@ class SecretsConfig
     static string $cloudFlareZoneId = '';
     static string $cloudFlareApiKey = '';
     static string $yahooClientId = '';
+    static string $openaiApiKey = '';
 }
