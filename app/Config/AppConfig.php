793d01d3ceb2b882f2d9085a36ca050a3978f9e2
mimimiku778 <0203.sub@gmail.com>
2025-07-29 03:20:53 +0900

    タグリストの制限を追加し、表示するタグ数を設定

--- app/Config/AppConfig.php
+++ app/Config/AppConfig.php
@@ -30,6 +30,7 @@ class AppConfig
     const LINE_OPEN_URL_SUFFIX = '?utm_source=line-openchat-seo&utm_medium=category&utm_campaign=default';
 
     static int $listLimitTopRanking = 10;
+    static int $tagListLimit = 3;
     const LIST_LIMIT_MY_LIST = 50;
     const LIST_LIMIT_RECENT_COMMENT = 50;
     const LIST_LIMIT_RECENTLY_REGISTERED = 100;
--- app/Views/components/tag_list_section.php
+++ app/Views/components/tag_list_section.php
@@ -1,6 +1,10 @@
 <section class="tag-list-section">
     <ul class="tag-list">
-        <?php foreach ($tags as $key => $word) : ?>
+        <?php
+
+use App\Config\AppConfig;
+
+ foreach (array_slice($tags, 0, AppConfig::$tagListLimit) as $key => $word) : ?>
             <li>
                 <a class="tag-btn" href="<?php echo url('recommend/' . urlencode(htmlspecialchars_decode($word))) ?>">
                     <?php echo \App\Services\Recommend\TagDefinition\Ja\RecommendUtility::extractTag($word) ?>
