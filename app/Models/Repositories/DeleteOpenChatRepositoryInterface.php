8be5a4e8c3fc637cc9eafbc9a3cbafd9c84c91d8
mimimiku778 <0203.sub@gmail.com>
2025-08-19 22:58:08 +0900

    DeleteOpenChatRepositoryにinsertDeletedOpenChatメソッドを追加し、関連するインターフェースを更新

--- app/Models/Repositories/DeleteOpenChatRepositoryInterface.php
+++ app/Models/Repositories/DeleteOpenChatRepositoryInterface.php
@@ -7,4 +7,6 @@ namespace App\Models\Repositories;
 interface DeleteOpenChatRepositoryInterface
 {
     public function deleteOpenChat(int $open_chat_id): bool;
+
+    public function insertDeletedOpenChat(int $open_chat_id, string $emid): void;
 }
