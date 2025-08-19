47befc4f9768153815bb43a55cc00c16e1233eec
mimimiku778 <0203.sub@gmail.com>
2025-08-19 23:04:46 +0900

    ApiDeletedOpenChatListRepositoryのクエリを修正し、open_chat_deletedテーブルの結合条件を更新。ApiOpenChatDeleterにDeleteOpenChatRepositoryInterfaceを追加し、削除処理を改善。

--- app/Services/OpenChat/Api/ApiOpenChatDeleter.php
+++ app/Services/OpenChat/Api/ApiOpenChatDeleter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Services\OpenChat\Api;
 
+use App\Models\Repositories\DeleteOpenChatRepositoryInterface;
 use App\Services\OpenChat\Crawler\OpenChatUrlChecker;
 use App\Services\OpenChat\Dto\OpenChatRepositoryDto;
 use App\Services\OpenChat\Updater\OpenChatDeleter;
@@ -14,11 +15,15 @@ class ApiOpenChatDeleter implements OpenChatDeleterInterface
     function __construct(
         private OpenChatDeleter $openChatDeleter,
         private OpenChatUrlChecker $openChatUrlChecker,
+        private DeleteOpenChatRepositoryInterface $deleteOpenChatRepository,
     ) {}
 
     function deleteOpenChat(OpenChatRepositoryDto $repoDto): void
     {
         $this->openChatDeleter->deleteOpenChat($repoDto);
-        $this->openChatUrlChecker->isOpenChatUrlAvailable($repoDto->invitationTicket);
+
+        if (!$this->openChatUrlChecker->isOpenChatUrlAvailable($repoDto->invitationTicket)) {
+            $this->deleteOpenChatRepository->insertDeletedOpenChat($repoDto->open_chat_id, '');
+        }
     }
 }
