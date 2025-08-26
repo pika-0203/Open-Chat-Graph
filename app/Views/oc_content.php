ee9e0ffd4ef28d2cb12cae350c8a7e49d973c3e0
mimimiku778 <0203.sub@gmail.com>
2025-08-26 20:49:18 +0900

    広告スロットの出力を修正し、'siteSeparatorResponsive'から'ocSeparatorResponsive'に変更

--- app/Views/oc_content.php
+++ app/Views/oc_content.php
@@ -213,7 +213,7 @@ viewComponent('oc_head', compact('_css', '_meta', '_schema', '_chartArgDto', '_c
         <?php $recommendDto1 = $recommend[0] ?: $recommend[3] ?>
         <?php viewComponent('recommend_list2', ['recommend' => $recommendDto1, 'member' => $oc['member'], 'tag' => $recommend[2], 'id' => $oc['id'], 'showTags' => true, 'disableGAd' => true]) ?>
       </aside>
-      <?php GAd::output(GAd::AD_SLOTS['siteSeparatorResponsive'])
+      <?php GAd::output(GAd::AD_SLOTS['ocSeparatorResponsive'])
       ?>
     <?php endif ?>
 
